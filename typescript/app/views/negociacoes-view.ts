7d96f56fd00a71556f99a44cb508252dd44c4601
Matheus Reinheimer <matheus@mavetec.com.br>
2023-12-18 13:42:42 -0300

    finalização do 3° capitulo

--- typescript/app/views/negociacoes-view.ts
+++ typescript/app/views/negociacoes-view.ts
@@ -4,7 +4,7 @@ import { View } from "./view.js";
 export class NegociacoesView extends View<Negociacoes> {
 
     // Metodo template sempre retorna String
-    template(model: Negociacoes): string {
+    protected template(model: Negociacoes): string {
         return `
         <table class="table table-hover table-bordered">
             <thead>
@@ -18,7 +18,7 @@ export class NegociacoesView extends View<Negociacoes> {
                 ${model.lista().map(negociacao => {
                     return `
                         <tr>
-                            <td>${new Intl.DateTimeFormat().format(negociacao.data)}</td>
+                            <td>${this.formatar(negociacao.data)}</td>
                             <td>${negociacao.quantidade}</td>
                             <td>${negociacao.valor}</td>
                         </tr>
@@ -26,6 +26,10 @@ export class NegociacoesView extends View<Negociacoes> {
                 }).join('')}
             </tbody>
         </table>
-        `
+        `;
+    }
+
+    private formatar(data: Date) {
+        new Intl.DateTimeFormat().format(data);
     }
 }
