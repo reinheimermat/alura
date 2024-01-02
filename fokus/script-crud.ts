9922eaa5afdcff91142ea74288f4b0f7b8e80e25
Matheus Reinheimer <matheus@mavetec.com.br>
2024-01-02 13:42:30 -0300

    aula 02 - Imutabilidade e estado inicial.

--- fokus/script-crud.ts
+++ fokus/script-crud.ts
@@ -1,9 +1,27 @@
 export interface Tarefa {
-    titulo: string
+    descricao: string
     concluida: boolean
 }
 
 export interface EstadoAplicacao {
     tarefas: Tarefa[]
-    tarefaSelecionada: Tarefa
+    tarefaSelecionada: Tarefa | null
+}
+
+let estado: EstadoAplicacao = {
+    tarefas: [
+        {
+            descricao: 'Tarefa concluída',
+            concluida: true
+        },
+        {
+            descricao: 'Tarefa pendente 1',
+            concluida: false
+        },
+        {
+            descricao: 'Tarefa pendente 2',
+            concluida: false
+        }
+    ],
+    tarefaSelecionada: null
 }
